d3deaf151f194e5bbe8a0a1cef237b0bf7edb018
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2018-12-04 15:40:04 +0530

    Content Version 4
    Added one black block.

--- child.js
+++ child.js
@@ -1,15 +1,14 @@
-
-(function() {
+(function () {
 
 	function init(item) {
 		var items = item.querySelectorAll('li'),
-        current = 0,
-        autoUpdate = true,
-        timeTrans = 4000;
-        
+			current = 0,
+			autoUpdate = true,
+			timeTrans = 4000;
+
 		//create nav
-		var nav = document.createElement('nav');
-		nav.className = 'nav_arrows';
+		// var nav = document.createElement('nav');
+		// nav.className = 'nav_arrows';
 
 		//create button prev
 		var prevbtn = document.createElement('button');
@@ -24,29 +23,29 @@
 		//create counter
 		var counter = document.createElement('div');
 		counter.className = 'counter';
-		counter.innerHTML = "<span>1</span><span>"+items.length+"</span>";
+		counter.innerHTML = "<span>1</span><span>" + items.length + "</span>";
 
-		if (items.length > 1) {
-			nav.appendChild(prevbtn);
-			nav.appendChild(counter);
-			nav.appendChild(nextbtn);
-			item.appendChild(nav);
-		}
+		// if (items.length > 1) {
+		// 	nav.appendChild(prevbtn);
+		// 	nav.appendChild(counter);
+		// 	nav.appendChild(nextbtn);
+		// 	item.appendChild(nav);
+		// }
 
 		items[current].className = "current";
-		if (items.length > 1) items[items.length-1].className = "prev_slide";
+		if (items.length > 1) items[items.length - 1].className = "prev_slide";
 
-		var navigate = function(dir) {
+		var navigate = function (dir) {
 			items[current].className = "";
 
 			if (dir === 'right') {
-				current = current < items.length-1 ? current + 1 : 0;
+				current = current < items.length - 1 ? current + 1 : 0;
 			} else {
-				current = current > 0 ? current - 1 : items.length-1;
+				current = current > 0 ? current - 1 : items.length - 1;
 			}
 
-			var	nextCurrent = current < items.length-1 ? current + 1 : 0,
-				prevCurrent = current > 0 ? current - 1 : items.length-1;
+			var nextCurrent = current < items.length - 1 ? current + 1 : 0,
+				prevCurrent = current > 0 ? current - 1 : items.length - 1;
 
 			items[current].className = "current";
 			items[prevCurrent].className = "prev_slide";
@@ -55,29 +54,29 @@
 			//update counter
 			counter.firstChild.textContent = current + 1;
 		}
-    
-    item.addEventListener('mouseenter', function() {
-			autoUpdate = false;
-		});
 
-		item.addEventListener('mouseleave', function() {
-			autoUpdate = true;
-		});
+		// item.addEventListener('mouseenter', function () {
+		// 	autoUpdate = false;
+		// });
 
-		setInterval(function() {
+		// item.addEventListener('mouseleave', function () {
+		// 	autoUpdate = true;
+		// });
+
+		setInterval(function () {
 			if (autoUpdate) navigate('right');
-		},timeTrans);
-    
-		prevbtn.addEventListener('click', function() {
+		}, timeTrans);
+
+		prevbtn.addEventListener('click', function () {
 			navigate('left');
 		});
 
-		nextbtn.addEventListener('click', function() {
+		nextbtn.addEventListener('click', function () {
 			navigate('right');
 		});
 
 		//keyboard navigation
-		document.addEventListener('keydown', function(ev) {
+		document.addEventListener('keydown', function (ev) {
 			var keyCode = ev.keyCode || ev.which;
 			switch (keyCode) {
 				case 37:
@@ -91,16 +90,18 @@
 
 		// swipe navigation
 		// from http://stackoverflow.com/a/23230280
-		item.addEventListener('touchstart', handleTouchStart, false);        
+		item.addEventListener('touchstart', handleTouchStart, false);
 		item.addEventListener('touchmove', handleTouchMove, false);
 		var xDown = null;
 		var yDown = null;
+
 		function handleTouchStart(evt) {
 			xDown = evt.touches[0].clientX;
 			yDown = evt.touches[0].clientY;
 		};
+
 		function handleTouchMove(evt) {
-			if ( ! xDown || ! yDown ) {
+			if (!xDown || !yDown) {
 				return;
 			}
 
@@ -110,14 +111,14 @@
 			var xDiff = xDown - xUp;
 			var yDiff = yDown - yUp;
 
-			if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-				if ( xDiff > 0 ) {
+			if (Math.abs(xDiff) > Math.abs(yDiff)) { /*most significant*/
+				if (xDiff > 0) {
 					/* left swipe */
 					navigate('right');
 				} else {
 					navigate('left');
 				}
-			} 
+			}
 			/* reset values */
 			xDown = null;
 			yDown = null;
@@ -126,7 +127,7 @@
 
 	}
 
-	[].slice.call(document.querySelectorAll('.cd-slider')).forEach( function(item) {
+	[].slice.call(document.querySelectorAll('.cd-slider')).forEach(function (item) {
 		init(item);
 	});
 
